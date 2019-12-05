7df1ec5ecef3368f60087ec818f976c10f440878
Hikman ganda sasmita <hikman.lord.leader@gmail.com>
2019-12-05 20:52:24 +0700

    ganti lagi

--- www/js/index.js
+++ www/js/index.js
@@ -19,18 +19,22 @@
 var app = {
     // Application Constructor
     initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        this.bindEvents();
     },
-
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
     // deviceready Event Handler
     //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-        document.getElementById('openBrowser').click();
+        app.receivedEvent('deviceready');
     },
-
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
@@ -43,64 +47,3 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-
-app.initialize();
-
-
-
-document.getElementById("openBrowser").addEventListener("click", openBrowser);
-
-document.addEventListener("offline", onOffline, false);
-
-function onOffline() {
-    // alert('onOffline');
-    testShareSheet();
-}
-
-function openBrowser() {
-   var url = 'http://sikopritmis.bogorkab.go.id/';
-   var target = '_blank';
-   var options = "location=no,toolbar=no"
-   var ref = cordova.InAppBrowser.open(url, target, options);
-
-   ref.addEventListener('loadstart', loadstartCallback);
-   ref.addEventListener('loadstop', loadstopCallback);
-   ref.addEventListener('loadloaderror', loaderrorCallback);
-   ref.addEventListener('exit', exitCallback);
-
-   function loadstartCallback(event) {
-     window.plugins.spinnerDialog.show();
-   }
-
-   function loadstopCallback(event) {
-      window.plugins.spinnerDialog.hide();
-   }
-
-   function loaderrorCallback(error) {
-      testShareSheet();
-   }
-
-   function exitCallback() {
-      console.log('Browser is closed...')
-   }
-}
-
-
-var callback = function(buttonIndex) {
-    setTimeout(function() {
-      document.getElementById('openBrowser').click();
-    });
-  };
-  function testShareSheet() {
-    var options = {
-      androidTheme : window.plugins.actionsheet.ANDROID_THEMES.THEME_DEVICE_DEFAULT_LIGHT, // material
-      title: 'Network error! Please check your Internet connection',
-      buttonLabels: ['Reload'],
-      addCancelButtonWithLabel: 'Cancel',
-      androidEnableCancelButton : true,
-      winphoneEnableCancelButton : true,
-      iosEnableCancelButton : true,
-      destructiveButtonLast: true // you can choose where the destructive button is shown
-    };
-    window.plugins.actionsheet.show(options, callback);
-  }
